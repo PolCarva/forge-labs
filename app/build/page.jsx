935776002894a91eb48527929e57e92e75cd26c7
PolCarva <pablocarvalhogimenez@gmail.com>
2024-07-22 17:41:39 -0300

    chore: Add lazy loading for external script in Home component

--- app/build/page.jsx
+++ app/build/page.jsx
@@ -1,12 +1,22 @@
-"use client"
+"use client";
+
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://unpkg.com/@splinetool/viewer@1.9.2/build/spline-viewer.js";
+    script.type = "module";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <main className="max-h-[calc(100svh-104px)]">
-      <script
-        type="module"
-        src="https://unpkg.com/@splinetool/viewer@1.9.2/build/spline-viewer.js"
-      ></script>
       <spline-viewer url="https://prod.spline.design/MvAIzM0G4x17REUv/scene.splinecode"></spline-viewer>
     </main>
   );
