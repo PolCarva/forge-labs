05732889c30f920eac0aeb883c37953fa5e76c73
PolCarva <pablocarvalhogimenez@gmail.com>
2024-07-23 19:08:18 -0300

    chore: Add file-loader rule for .glb and .gltf files in webpack config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
-};
-
-export default nextConfig;
+    webpack: (config, { isServer }) => {
+      // Agregar regla para cargar archivos .glb y .gltf
+      config.module.rules.push({
+        test: /\.(glb|gltf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'static/models/',
+              publicPath: '/_next/static/models/',
+            },
+          },
+        ],
+      });
+  
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
