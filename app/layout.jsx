69d5c5a6bb58071578a342b48d56954eeec80f4c
PolCarva <pablocarvalhogimenez@gmail.com>
2024-07-25 18:49:48 -0300

    Fix 503 layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import "./globals.css";
 import Footer from "@/components/Footer";
+import Image from "next/image";
 
 export const metadata = {
   title: "Forge Labs - Construye tu PC a medida",
@@ -13,9 +14,27 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={""}>
-        <Header />
-        {children}
-        <Footer />
+        <div className="hidden lg:block">
+          <Header />
+          {children}
+          <Footer />
+        </div>
+        <div className="h-svh w-svw lg:hidden grid bg-black place-content-center">
+          <div className="flex flex-col gap-5 items-center text-white">
+            <Image
+              src={"/assets/logo_small.svg"}
+              alt="Logo"
+              className="brightness-0 invert"
+              width={100}
+              height={100}
+            />
+            <h2 className="text-9xl text-center font-bold">503</h2>
+            <p className="text-xl text-center px-5">
+              La página mobile se está forjando{" "}
+              <span className="block">Por favor entrar desde la PC</span>
+            </p>
+          </div>
+        </div>
       </body>
     </html>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -4,31 +4,17 @@ import Hero from "@/components/Hero";
 import ImageAndText from "@/components/ImageAndText";
 import Spacer from "@/components/Spacer";
 import ChatBotBubble from "@/components/ui/ChatBotBubble";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="">
-      <Hero />
-      <ImageAndText />
-      <Spacer />
-      <FeaturedProducts />
-      <BuildYourPc />
-      <ChatBotBubble />
-      <div className="fixed inset-0 w-svh h-svh overflow-hidden lg:hidden grid bg-black place-content-center">
-        <div className="flex flex-col gap-5 items-center text-white">
-          <Image
-            src={"/assets/logo_small.svg"}
-            alt="Logo"
-            className="brightness-0 invert"
-            width={100}
-            height={100}
-          />
-          <h2 className="text-9xl font-bold">503</h2>
-          <p className="text-4xl">La página mobile se está forjando</p>
-          <p className="text-4xl">Por favor entrar desde la PC</p>
-        </div>
-      </div>
+        <Hero />
+        <ImageAndText />
+        <Spacer />
+        <FeaturedProducts />
+        <BuildYourPc />
+        <ChatBotBubble />
+      
     </main>
   );
 }
